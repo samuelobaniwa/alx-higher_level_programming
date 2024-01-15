8e7dee87400de34b915c79b48ba99125e2aab152
samuelobaniwa <treasureobaniwa@gmail.com>
2024-01-15 03:15:53 -0800

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,13 +2,11 @@
 #include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
-
 void print_python_bytes(PyObject *p)
 {
 	long int size;
 	int i;
 	char *trying_str = NULL;
-
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
