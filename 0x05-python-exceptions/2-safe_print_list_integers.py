f4fe5846dcc6c37d94737d48b8adcca8f6526c2b
samuelobaniwa <treasureobaniwa@gmail.com>
2024-01-22 16:28:54 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    Returns:
     count = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
