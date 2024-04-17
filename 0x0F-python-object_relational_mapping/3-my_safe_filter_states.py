18d87e10089a624f9b2ac14a30bed223163ece8e
samuelobaniwa <treasureobaniwa@gmail.com>
2024-04-17 09:07:03 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
-import sys
-import MySQLdb
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
+
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-    # Get the command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    rows_selected = db_cursor.fetchall()
 
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Prepare the SQL query with placeholders
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query with the state name as a parameter
-    cursor.execute(sql_query, (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
+    for row in rows_selected:
         print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
-
